8922d122faa99a4ea571dd817c3f45c050dc215b
alfredkoshan <alfred.koshan@gmail.com>
2013-11-15 20:30:56 -0800

    Transportation(CarAgent)

--- src/simcity/CarAgent.java
+++ src/simcity/CarAgent.java
@@ -1,11 +1,59 @@
 package simcity;
 
-public class CarAgent {
+import java.awt.Point;
 
-	private String destination;
+import agent.Agent;
+//Car Agent
+//TODO, it will ask transportation to know how to move to destination
+// (must pass through some points)
+public class CarAgent extends Agent{
+
+	private Point destination;
+	private String destinationString;
 	
-	public void msgGoToDestination(String d) {
-		destination = d;
+	public enum AgentState {
+
+		DoingNothing, GoingToDestination
+	};
+
+	private AgentState state = AgentState.DoingNothing;// The start state
+
+	public enum AgentEvent {
+
+		none, started, gotDestination
+		
+	};
+
+	private AgentEvent event = AgentEvent.none;
+	
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		if (state ==  AgentState.DoingNothing && event == AgentEvent.started){
+			state = AgentState.GoingToDestination;
+			//TODO ask CarGUI to move
+			return true;
+		}
+		if (state ==  AgentState.GoingToDestination && event == AgentEvent.gotDestination){
+			state = AgentState.DoingNothing;
+			return true;
+		}
+		return false;
 	}
 
+	//start to run to point
+	public void Start(Point point) {
+		this.destination = point;
+		event = AgentEvent.started;
+		stateChanged();
+	}
+	
+	//called by Car GUI to know it reached destination
+	public void msgGotDestination() {
+		event = AgentEvent.gotDestination;
+		stateChanged();
+	}
+
+	public void msgGoToDestination(String d) {
+		destinationString = d;		
+	}
 }
