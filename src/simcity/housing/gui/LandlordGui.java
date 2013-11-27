6cc57aeff4e373f00b9fdf43dc1339fbb0a0d0ba
Sarah Fair <sarahfai@usc.edu>
2013-11-26 20:53:18 -0800

    Housing Guis complete and movement system unit tested. close #16. close #39

--- src/simcity/housing/gui/LandlordGui.java
+++ src/simcity/housing/gui/LandlordGui.java
@@ -1,43 +1,327 @@
 package simcity.housing.gui;
 
 import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import simcity.gui.Gui;
 import simcity.housing.LandlordRole;
+import simcity.interfaces.HousingGuiInterface;
+import simcity.interfaces.Resident;
+import simcity.mock.EventLog;
+import simcity.mock.LoggedEvent;
 
 public class LandlordGui implements Gui
 {
+	LandlordRole landlord;
+	HousingGuiInterface gui;
+
+	public int xPos;
+	public int yPos;
+	public int xDestination, yDestination;
+	public int xGoal;
+	public int yGoal;
+	private int dimensions;
+	public int boxY;
+	public int boxX;
+	public List<MoveBox> pastBoxes = new ArrayList<MoveBox>();
+	public MoveBox currentBox;
+	private boolean exiting;
+
+	public enum Command
+	{
+		exit,
+		noCommand;
+	}
+	public Command command = Command.noCommand;
+
+	public EventLog log = new EventLog();
 
 	public LandlordGui(LandlordRole l)
 	{
-		// TODO Auto-generated constructor stub
+		landlord = l;
+		xPos = 500;
+		yPos = 220;
+		xDestination = xPos;
+		yDestination = yPos;
+		xGoal = xPos;
+		yGoal = yPos;
+		boxY = 11;
+		boxX = 25;
+		dimensions = 20;
+		exiting = false;
+		command = Command.noCommand;
+	}
+	public void setGui(HousingGuiInterface gui)
+	{
+		this.gui = gui;
 	}
 
-	@Override
 	public void updatePosition()
 	{
-		// TODO Auto-generated method stub
-		
+		log.add(new LoggedEvent("Received updatePosition"));
+
+		if(exiting)
+		{
+			if (xPos < 500)
+			{
+				xPos++;
+			}
+			else
+			{
+				xPos = 500;
+				yPos = 220;
+				xDestination = xPos;
+				yDestination = yPos;
+				xGoal = xPos;
+				yGoal = yPos;
+				boxY = 11;
+				boxX = 25;
+				exiting = false;
+				command = Command.noCommand;
+				currentBox = null;
+
+				landlord.msgAtLocation();
+			}
+		}
+		else
+		{
+			if(xPos == xDestination && yPos == yDestination && (xDestination != xGoal || yDestination != yGoal))
+			{
+				findMoveBox();
+			}
+
+			if (xPos < xDestination)
+			{
+				xPos++;
+			}
+			else if (xPos > xDestination)
+			{
+				xPos--;
+			}
+
+			if (yPos < yDestination)
+			{
+				yPos++;
+			}
+			else if (yPos > yDestination)
+			{
+				yPos--;
+			}
+
+			if (xPos == xGoal && yPos == yGoal)
+			{
+				pastBoxes.clear();
+				if(command == Command.exit)
+				{
+					exiting = true; 
+				}
+				else
+				{
+					landlord.msgAtLocation();
+				}
+			}
+		}
 	}
 
-	@Override
 	public void draw(Graphics2D g)
 	{
-		// TODO Auto-generated method stub
-		
+		g.setColor(Color.MAGENTA);
+		g.fillRect(xPos, yPos, dimensions, dimensions);
+	}
+
+	public HousingGuiInterface getCurrentHouse()
+	{
+		return gui;
 	}
 
 	@Override
 	public boolean isPresent()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
-	public void setGui(HousingGui gui)
+	public void doGoToLocation(Point p, String purpose)
 	{
-		// TODO Auto-generated method stub
-		
+		log.add(new LoggedEvent("Received doGoToLocation"));
+
+		xGoal = (int) p.getX();
+		yGoal = (int) p.getY();
+
+		if(purpose.equals("Exit"))
+		{
+			command = Command.exit;
+		}
+		else
+		{
+			command = Command.noCommand;
+		}
 	}
 
+	public void findMoveBox()
+	{
+		log.add(new LoggedEvent("Received findMoveBox"));
+
+		List<MoveBox> boxesToCheck = new ArrayList<MoveBox>();
+
+		if (xPos < xGoal)
+		{
+			if((boxX + 1) < 25)
+			{
+				boxesToCheck.add(gui.getBox(boxY, boxX + 1));
+			}
+		}
+		else if (xPos > xGoal)
+		{
+			if((boxX - 1) >= 0)
+			{
+				boxesToCheck.add(gui.getBox(boxY, boxX - 1));
+			}
+		}
+		if(boxX < 25)
+		{
+			if (yPos > yGoal)
+			{
+				if((boxY - 1) >= 0)
+				{
+					boxesToCheck.add(gui.getBox(boxY - 1, boxX));
+				}
+			}
+			else if (yPos < yGoal)
+			{
+				if((boxY + 1) < 25)
+				{
+					boxesToCheck.add(gui.getBox(boxY + 1, boxX));
+				}
+			}
+		}
+
+		if (xPos == xGoal)
+		{
+			if((boxX + 1) < 25)
+			{
+				boxesToCheck.add(gui.getBox(boxY, boxX + 1));
+			}
+			if((boxX - 1) >= 0)
+			{
+				boxesToCheck.add(gui.getBox(boxY, boxX - 1));
+			}
+		}
+		if (yPos == yGoal)
+		{
+			if((boxY + 1) < 25)
+			{
+				boxesToCheck.add(gui.getBox(boxY + 1, boxX));
+			}
+			if((boxY - 1) >= 0)
+			{
+				boxesToCheck.add(gui.getBox(boxY - 1, boxX));
+			}
+		}
+		if(currentBox != null)
+		{
+			currentBox.setOpen(true);
+			boxesToCheck.add(currentBox);
+			pastBoxes.add(currentBox);
+			currentBox = null;
+		}
+		List<MoveBox> untouchedBoxes = new ArrayList<MoveBox>();
+		for(MoveBox b : boxesToCheck)
+		{
+			boolean untouched = true;
+			for(MoveBox pb : pastBoxes)
+			{
+				if(b == pb)
+				{
+					untouched = false;
+				}
+			}
+			if(untouched)
+			{
+				untouchedBoxes.add(b);
+			}
+		}
+		for(MoveBox b : untouchedBoxes)
+		{
+			if(b.getOpen())
+			{
+				currentBox = b;
+				b.setOpen(false);
+				boxX = b.getIndexX();
+				boxY = b.getIndexY();
+				break;
+			}
+		}
+		if(currentBox == null)
+		{
+			gui.setBox(boxesToCheck);
+			boxesToCheck.clear();
+
+			if(boxX < 25)
+			{
+				if (yPos < yGoal)
+				{
+					if((boxY - 1) >= 0)
+					{
+						boxesToCheck.add(gui.getBox(boxY - 1, boxX));
+					}
+				}
+				else if (yPos > yGoal)
+				{
+					if((boxY + 1) < 25)
+					{
+						boxesToCheck.add(gui.getBox(boxY + 1, boxX));
+					}
+				}
+			}
+			if (xPos < xGoal)
+			{
+				if((boxX - 1) >= 0)
+				{
+					boxesToCheck.add(gui.getBox(boxY, boxX - 1));
+				}
+			}
+			else if (xPos > xGoal)
+			{
+				if((boxX + 1) < 25)
+				{
+					boxesToCheck.add(gui.getBox(boxY, boxX + 1));
+				}
+			}
+
+			untouchedBoxes.clear();
+			for(MoveBox b : boxesToCheck)
+			{
+				boolean untouched = true;
+				for(MoveBox pb : pastBoxes)
+				{
+					if(b == pb)
+					{
+						untouched = false;
+					}
+				}
+				if(untouched)
+				{
+					untouchedBoxes.add(b);
+				}
+			}
+			for(MoveBox b : untouchedBoxes)
+			{
+				if(b.getOpen())
+				{
+					b.setOpen(false);
+					boxX = b.getIndexX();
+					boxY = b.getIndexY();
+					currentBox = b;
+					break;
+				}
+			}
+		}
+		boxesToCheck.remove(currentBox);
+		gui.setBox(boxesToCheck);
+		xDestination = currentBox.getX();
+		yDestination = currentBox.getY();
+	}
 }
