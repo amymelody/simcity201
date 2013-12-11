8002eebd56348cc77c249955cd92429062e61f36
Jesus Garcia <jesusega@usc.edu>
2013-12-10 20:30:29 -0800

    Updates on MarketInputPanel.

--- src/simcity/market/gui/MarketInputPanel.java
+++ src/simcity/market/gui/MarketInputPanel.java
@@ -11,6 +11,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import simcity.bank.gui.BankManagerGui;
 import simcity.gui.DelivererGui;
@@ -19,7 +20,7 @@ import simcity.market.MarketEmployeeRole;
 import simcity.market.MarketCustomerRole;
 import simcity.market.MarketCashierRole;
 
-public class MarketInputPanel extends JPanel
+public class MarketInputPanel extends JPanel implements ActionListener
 {
 	static final int LSPACE = 10;
 	static final int GROWS = 3;
@@ -29,7 +30,10 @@ public class MarketInputPanel extends JPanel
 	public MarketGui marketGui;
 	public JButton goBack;
 	private JPanel marketLabel = new JPanel();
+	private JPanel inventoryLabel = new JPanel();
+	private JTabbedPane group = new JTabbedPane();
     JLabel marketInfo = new JLabel();
+    JLabel inventoryInfo = new JLabel();
     private MarketCashierRole cashier;
     private MarketCashierGui cashierGui;
 	private Vector<MarketEmployeeRole> employees = new Vector<MarketEmployeeRole>();
@@ -51,9 +55,17 @@ public class MarketInputPanel extends JPanel
         setBorder(BorderFactory.createBevelBorder(0));
         
         initMarketLabel();
-        add(marketLabel, BorderLayout.CENTER);
+        group.addTab("Menu", null, marketLabel, "Menu");
+        initMarketInv();
+        group.addTab("Inventory", null, inventoryLabel, "Inventory");
+        group.setSelectedIndex(0);
+        
+        add(group, BorderLayout.CENTER);
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		initMarketInv();
+	}
 	private String getMName() {
 		if(name == "market1")
 			return "Market 1";
@@ -67,6 +79,32 @@ public class MarketInputPanel extends JPanel
 		else if(name == "market2")
 			return "blue";
 		return null;
+	}
+	private void initMarketInv() {
+		inventoryLabel.setLayout(new BorderLayout());
+        inventoryInfo.setText(
+                "<html>"
+                + "<h3 color=" + getColor() + ">Welcome to " + getMName() + "!</h3>"
+                		+ "<h4><u> Inventory</u></h4><table cellspacing=0 color=" + getColor() + ">"
+                		+ "<tr><td>Salad</td><td>" + cashier.inventory.getAmount("Salad") + "</td></tr>"
+                		+ "<tr><td>Steak</td><td>" + cashier.inventory.getAmount("Steak") + "</td></tr>"
+                		+ "<tr><td>Pizza</td><td>" + cashier.inventory.getAmount("Pizza") + "</td></tr>"
+                		+ "<tr><td>Chicken</td><td>" + cashier.inventory.getAmount("Chicken") + "</td></tr>"
+                		+ "<tr><td>Spaghetti</td><td>" + cashier.inventory.getAmount("Spaghetti") + "</td></tr>"
+                		+ "<tr><td>Lasagna</td><td>" + cashier.inventory.getAmount("Lasagna") + "</td></tr>"
+                		+ "<tr><td>Garlic Bread</td><td>" + cashier.inventory.getAmount("Garlic Bread") + "</td></tr>"
+                		+ "<tr><td>Ribs</td><td>" + cashier.inventory.getAmount("Ribs") + "</td></tr>"
+                		+ "<tr><td>Burger</td><td>" + cashier.inventory.getAmount("Burger") + "</td></tr>"
+                		+ "<tr><td>Enchiladas</td><td>" + cashier.inventory.getAmount("Enchiladas") + "</td></tr>"
+                		+ "<tr><td>Tacos</td><td>" + cashier.inventory.getAmount("Tacos") + "</td></tr>"
+                		+ "<tr><td>Pozole</td><td>" + cashier.inventory.getAmount("Pozole") + "</td></tr>"
+                		+ "<tr><td>Horchata</td><td>" + cashier.inventory.getAmount("Horchata") + "</td></tr>"
+                		+ "</table><br></html>");
+
+        inventoryLabel.setBorder(BorderFactory.createBevelBorder(0));
+        inventoryLabel.add(inventoryInfo, BorderLayout.CENTER);
+        inventoryLabel.add(new JLabel(" "), BorderLayout.EAST);
+        inventoryLabel.add(new JLabel(" "), BorderLayout.WEST);
 	}
 	private void initMarketLabel() {
         marketLabel.setLayout(new BorderLayout());
@@ -89,11 +127,12 @@ public class MarketInputPanel extends JPanel
                 		+ "<tr><td>Horchata</td><td>$3.00</td></tr>"
                 		+ "</table><br></html>");
 
-        //marketLabel.setBorder(BorderFactory.createBevelBorder(0));
+        marketLabel.setBorder(BorderFactory.createBevelBorder(0));
         marketLabel.add(marketInfo, BorderLayout.CENTER);
-        marketLabel.add(new JLabel("      "), BorderLayout.EAST);
-        marketLabel.add(new JLabel("      "), BorderLayout.WEST);
-    }
+        marketLabel.add(new JLabel(" "), BorderLayout.EAST);
+        marketLabel.add(new JLabel(" "), BorderLayout.WEST);
+   
+	}
 	
 	public void setCashier(MarketCashierRole cashier) {
 		this.cashier = cashier;
